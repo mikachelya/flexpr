0c806320c0044781b51e260b995010991c9937f7
mikachelya <mikachelya@gmail.com>
2026-07-21 17:47:37 +0200

    hix typos in help

--- help.h
+++ help.h
@@ -6,18 +6,18 @@ Options:\n\
   Precision:\n\
     These options determine which values are considered equal\n\
     -E, --epsilon=EPS    set epsilon value (default DBL_EPSILON, ~2.22e-16);\n\
-                         overriden by subsequent -T flags\n\
+                         overridden by subsequent -T flags\n\
     -T, --tolerance=TOL  set tolerance in terms of the number of double-\n\
                          precision values between the compared numbers;\n\
-                         overriden by subsequent -E flags\n\
+                         overridden by subsequent -E flags\n\
 \n\
   Output:\n\
     -D, --digits=NUM     round the output to the specified number of digits\n\
                          after the decimal point (rounds to even)\n\
-                         overriden by subsequent -I flags\n\
+                         overridden by subsequent -I flags\n\
     -I, --integer        rounds the output to an integer;\n\
                          equivalent to -D 0;\n\
-                         overriden by subsequent -D flags\n\
+                         overridden by subsequent -D flags\n\
     -L, --logical        mutes result printing, overriding -D and -I;\n\
                          exit with 1 if result is zero/false, 0 otherwise;\n\
                          truthiness evaluated based on EPS/TOL\n\
